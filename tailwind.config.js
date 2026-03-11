2ea0b6550458320d4a0e735095377befdcf9f100
Dinheiroemcaixa <suporte@dinheiroemcaixa.com.br>
2026-03-11 15:12:14 -0300

    fix: otimiza content do tailwind para acelerar o build e corrigir deploy

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,9 +2,12 @@
 export default {
   content: [
     "./index.html",
-    "./**/*.{js,ts,jsx,tsx}",
+    "./src/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
+    "./hooks/**/*.{js,ts,jsx,tsx}",
     "./services/**/*.{js,ts,jsx,tsx}",
+    "./utils/**/*.{js,ts,jsx,tsx}",
+    "./App.tsx",
   ],
   darkMode: 'class',
   theme: {
